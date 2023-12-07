c5c5876537db5c2ba221049f611d95ac1a040c6d
Andri Priyadi <dripriyadi@gmail.com>
2023-12-07 22:16:06 +0700

    upload done not tested

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 const { isLoggedIn } = require('../helpers/util');
 var router = express.Router();
-var moment = require('moment'); 
+var moment = require('moment');
 const path = require('path');
 
 
@@ -14,7 +14,7 @@ module.exports = function (db) {
     const limit = 5
     const offset = (page - 1) * 5
     let sortMode;
-    const {rows : profil} = await db.query(`SELECT * FROM "users" WHERE id = $1`, [req.session.user.userid])
+    const { rows: profil } = await db.query(`SELECT * FROM "users" WHERE id = $1`, [req.session.user.userid])
 
     params.push(req.session.user.userid)
     paramscount.push(req.session.user.userid)
@@ -62,10 +62,10 @@ module.exports = function (db) {
       else {
         const total = data.total;
         const pages = Math.ceil(total / limit);
-        db.query(sql, params, (err, { rows :data}) => {
-          if (err) res.send(err) 
-          else 
-          res.render('user/list', { data, query: req.query, moment, pages, offset, page, url: req.url, sortMode, profil: profil[0] })
+        db.query(sql, params, (err, { rows: data }) => {
+          if (err) res.send(err)
+          else
+            res.render('user/list', { data, query: req.query, moment, pages, offset, page, url: req.url, sortMode, profil: profil[0] })
 
         })
       }
@@ -81,32 +81,61 @@ module.exports = function (db) {
       res.redirect('/users')
     })
   })
-  router.get('/edit/:index', isLoggedIn,(req,res) =>{
+  router.get('/edit/:index', isLoggedIn, (req, res) => {
     const index = req.params.index;
-    db.query(`SELECT * FROM todos WHERE id = $1`, [index], (err,{rows : data})=>{
-      if(err) res.send(err)
-      else res.render('user/edit', {data, moment})
+    db.query(`SELECT * FROM todos WHERE id = $1`, [index], (err, { rows: data }) => {
+      if (err) res.send(err)
+      else res.render('user/edit', { data, moment })
     })
   })
 
-  router.post('/edit/:index', isLoggedIn, (req,res) =>{
+  router.post('/edit/:index', isLoggedIn, (req, res) => {
     const index = req.params.index;
-    const {title, deadline, complete} = req.body
-    db.query(`UPDATE todos SET title = $1, complete = $2, deadline = $3 WHERE id = $4 `, 
-    [title, Boolean(complete), deadline, index ], (err, data) =>{
-      if(err){
-        res.send(err)
-      } else res.redirect('/users')
-    })
+    const { title, deadline, complete } = req.body
+    db.query(`UPDATE todos SET title = $1, complete = $2, deadline = $3 WHERE id = $4 `,
+      [title, Boolean(complete), deadline, index], (err, data) => {
+        if (err) {
+          res.send(err)
+        } else res.redirect('/users')
+      })
   })
 
-  router.get('/delete/:index', isLoggedIn, (req,res) =>{
+  router.get('/delete/:index', isLoggedIn, (req, res) => {
     const index = req.params.index;
-    db.query(`DELETE FROM todos WHERE id = $1`, [index], (err) =>{
-      if(err) res.send(err)
+    db.query(`DELETE FROM todos WHERE id = $1`, [index], (err) => {
+      if (err) res.send(err)
       else res.redirect('/users')
     })
   })
+  router.get('/upload', isLoggedIn, (req, res) => {
+    res.render('user/upload')
+  })
+  router.post('/upload', isLoggedIn, (req, res) => {
 
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).send('No files were uploaded.');
+    }
+
+    const avatar = req.files.avatar;
+    const fileName = `${Date.now()}+ "-" + ${avatar.name}`
+    uploadPath = path.join(__dirname, '..', 'public', 'images', fileName);
+
+    // Use the mv() method to place the file somewhere on your server
+    avatar.mv(uploadPath, async function (err) {
+      if (err)
+        return res.status(500).send(err);
+      avatar.mv(uploadPath, async function (err) {
+        if (err)
+          return res.status(500).send(err);
+        try {
+          const { rows } = await db.query('UPDATE users SET avatar = $1 WHERE id = $2', [fileName, req.session.user.userid])
+          res.redirect('/users')
+        } catch (err) {
+          res.send(err)
+        }
+      });
+    })
+  });
   return router
 }
